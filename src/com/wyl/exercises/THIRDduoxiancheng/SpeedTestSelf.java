e45fe99d89e326afacb9019e3b272e96dd4f82a8
wendelinwerner <1058046323@qq.com>
2023-05-04 10:06:59 +0800

    高并发与多线程三
    Signed-off-by: wendelinwerner <1058046323@qq.com>

--- src/com/wyl/exercises/THIRDduoxiancheng/SpeedTestSelf.java
+++ src/com/wyl/exercises/THIRDduoxiancheng/SpeedTestSelf.java
@@ -1,6 +1,6 @@
 package com.wyl.exercises.THIRDduoxiancheng;
 
-import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.LongAdder;
 
@@ -10,29 +10,81 @@ public class SpeedTestSelf {
     static AtomicLong count1 = new AtomicLong(0L);
     static LongAdder count3 = new LongAdder();
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
+
         Thread[] threads = new Thread[1000];
 
-        for (int i = 0;i < threads.length;i++){
+        for (int i = 0; i < threads.length; i++) {
             threads[i] =
-                        new Thread(() ->    {
-                            for (int k = 0; k < 100000; k++)    count1.incrementAndGet();
-                        });
+                    new Thread(() -> {
+                        for (int k = 0; k < 100000; k++) count1.incrementAndGet();
+                    });
         }
 
         long start = System.currentTimeMillis();
 
         for (Thread t : threads) t.start();
 
-        for (Thread t : threads)t.join();
+        for (Thread t : threads) t.join();
 
         long end = System.currentTimeMillis();
 
-        System.out.println("Atomic:" + count1.get() + ",time:" + (end-start) + "ms");
+        System.out.println("Atomic:" + count1.get() + ",time:" + (end - start) + "ms");
+
+        //    --------------------------------------------------------------------------------------------------------------------
+
+        Object lock = new Object();
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i]  =
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            for (int k = 0;k < 100000;k++){
+                                    synchronized (lock) {
+                                        count2++;
+                                    }
+                            }
+                        }
+                    });
+        }
+
+        start = System.currentTimeMillis();
+
+        for (Thread t : threads) t.start();
+
+        for (Thread t : threads) t.join();
+
+        end = System.currentTimeMillis();
+
+        System.out.println("Sync:" + count2 + ",time:" + (end - start) + "ms");
+
+//--------------------------------------------------------------------------------------------------------
+
+        for (int i = 0; i < threads.length; i++){
+            threads[i] =
+                    new Thread(() -> {
+                        for (int k = 0; k < 100000; k++)
+                            count3.increment();
+                    });
+        }
+
+        start = System.currentTimeMillis();
+
+        for (Thread t : threads) t.start();
+
+        for (Thread t : threads) t.join();
+
+        end = System.currentTimeMillis();
+
+        System.out.println("LongAdder:" + count3 + ",time" + (end - start) + "ms");
+
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
-
-//    --------------------------------------------------------------------------------------------------------------------
-
-    Object lock = new Object();
 
 }
